497758aecd466ce526165c856ec7450d711c74e9
kottyan <exm6969@gmail.com>
2022-03-24 14:31:04 +0900

    職務経歴をアップデート

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const setInitalPage = () => {
   const container = document.getElementById('container');
-  const thisMonth = document.getElementById('thisMonth');
-  const toDay = new Date();
-  thisMonth.innerText = toDay.getFullYear() + ' / ' + ("0"+(toDay.getMonth() + 1)).slice(-2);
     setTimeout(() => {
       container.style.visibility = "visible";
       window.scrollTo(0,0);
